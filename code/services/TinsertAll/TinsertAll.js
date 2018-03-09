37e2cbda9c0872cb02038698f62bca21b3a890ce
Yash Jain <yashkjain1994@gmail.com>
2018-03-09 16:29:24 -0600

    Asset created by yashjain28

---
+++ code/services/TinsertAll/TinsertAll.js
@@ -0,0 +1,27 @@
+function TinsertAll(req, resp){
+    var bQ = new BigQuery(req.params.token);
+    var requestBody = {
+        "rows":
+            [
+                {
+                    "json":{
+                        "SrNo":"20",
+                        "Name":"YashJain",
+                        "City":"Mumbai"
+                    }
+                }
+            ]
+    }
+    var projectID = (req.params.projectID==="")?"gentle-impulse-161804":req.params.projectID;
+    var tableID = (req.params.tableID==="")?"testingTable":req.params.tableID;
+    var datasetID = (req.params.datasetID==="")?"new_ds":req.params.datasetID;
+    
+    bQ.insertAll(projectID, datasetID, tableID, requestBody, function(err, data){
+        if(err){
+            resp.error(data);
+        }
+        else{
+            resp.success(data);
+        }
+    });
+}
