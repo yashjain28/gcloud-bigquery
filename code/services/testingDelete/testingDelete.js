48b410148f70be9f05ce14712433f7be14b8d47c
Yash Jain <yashkjain1994@gmail.com>
2018-03-09 16:29:28 -0600

    Asset created by yashjain28

---
+++ code/services/testingDelete/testingDelete.js
@@ -0,0 +1,11 @@
+function testingDelete(req, resp){
+    var bQ = new BigQuery(req.params.token);
+    bQ.delete(req.params.projectID, req.params.datasetID, function(err, data){
+        if(err){
+            resp.error(data);
+        }
+        else{
+            resp.success(data);
+        }
+    });
+}
