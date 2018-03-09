b4882ca8352ea37c9fa1ee630eec0eca27dc4402
Yash Jain <yashkjain1994@gmail.com>
2018-03-09 16:29:23 -0600

    Asset created by yashjain28

---
+++ code/services/TGet/TGet.js
@@ -0,0 +1,11 @@
+function TGet(req, resp){
+    var bQ = new BigQuery(req.params.token);
+    bQ.get(req.params.projectID, req.params.datasetID, function(err, data){
+        if(err){
+            resp.error(data);
+        }
+        else{
+            resp.success(data);
+        }
+    });
+}
