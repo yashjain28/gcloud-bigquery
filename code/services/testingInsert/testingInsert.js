5ab84344cba73a889987d28584b2d0711049d91d
Yash Jain <yashkjain1994@gmail.com>
2018-03-09 16:29:29 -0600

    Asset created by yashjain28

---
+++ code/services/testingInsert/testingInsert.js
@@ -0,0 +1,19 @@
+function testingInsert(req, resp){
+    var bQ = new BigQuery(req.params.token);
+    var requestBody = {
+      "kind": "bigquery#dataset",
+  "id": "newDSS",
+  "datasetReference": {
+    "datasetId": "newDSS",
+    "projectId": "gentle-impulse-161804"
+  }
+};
+    bQ.insert(req.params.projectID, requestBody, function(err, data){
+        if(err){
+            resp.error(data);
+        }
+        else{
+            resp.success(data);
+        }
+    });
+}
