abeb13928c5c78ba3f69c4d8a419461dee667b7b
Yash Jain <yashkjain1994@gmail.com>
2018-03-09 16:29:26 -0600

    Asset created by yashjain28

---
+++ code/services/Tupdate/Tupdate.js
@@ -0,0 +1,26 @@
+function Tupdate(req, resp){
+    var bQ = new BigQuery(req.params.token);
+    var requestBody = {
+        "rows":
+            [
+                {
+                    "json":{
+                        "SrNo":"20",
+                        "Name":"YashJain",
+                        "City":"Mumbai"
+                    }
+                }
+            ]
+    }
+    var projectID = (req.params.projectID==="")?"gentle-impulse-161804":req.params.projectID;
+    var datasetID = (req.params.datasetID==="")?"new_ds":req.params.datasetID;
+    
+    bQ.update(projectID, datasetID, requestBody, function(err, data){
+        if(err){
+            resp.error(data);
+        }
+        else{
+            resp.success(data);
+        }
+    });
+}
